8c71539dc7713c7568ba459122768b07544941a1
kishankishore29 <kishankishore29@gmail.com>
2022-09-06 01:08:23 +0545

    remove_duplicate_from_sorted_array

--- easy/merge_two_sorted_lists.py
+++ easy/merge_two_sorted_lists.py
@@ -1,3 +1,5 @@
+# Problem Link: https://leetcode.com/problems/merge-two-sorted-lists/
+
 from typing import Optional
 
 
---
+++ easy/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,17 @@
+# Problem Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow, fast = 0, 1
+
+        while fast < len(nums):
+
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+            fast += 1
+
+        return slow + 1
